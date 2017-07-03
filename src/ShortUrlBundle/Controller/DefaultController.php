a20669e8e3b3b9c8d8dbcddb2859f66ee2c75ce2
Yannick Breuer <yanbreu@yroot.de>
2017-07-04 01:03:04 +0200

    Short Url Redirect

--- src/ShortUrlBundle/Controller/DefaultController.php
+++ src/ShortUrlBundle/Controller/DefaultController.php
@@ -4,14 +4,32 @@ namespace ShortUrlBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use ShortUrlBundle\Repository\ShortUrlRepository;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/{shortUrlIdent}")
      */
-    public function indexAction()
+    public function indexAction(Request $request, $shortUrlIdent)
     {
-        return $this->render('ShortUrlBundle:Default:index.html.twig');
+
+        $shortUrlRepo = $this->getDoctrine()->getRepository('ShortUrlBundle:ShortUrl');
+
+        /**
+         * @var \ShortUrlBundle\Entity\ShortUrl $shortUrl
+         */
+        $shortUrl = $shortUrlRepo->findOneBy([
+            'ident' => $shortUrlIdent
+        ]);
+
+        if($shortUrl === null) {
+            throw $this->createNotFoundException();
+        }
+        
+        return $this->redirect($shortUrl->getTargetUrl());
+
+
     }
 }
