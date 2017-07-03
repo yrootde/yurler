dce8848bb778d38e1ff9e530cf03c04e8b5c0ccc
Yannick Breuer <yanbreu@yroot.de>
2017-07-04 00:56:27 +0200

    docker env

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -40,12 +40,12 @@ class AppKernel extends Kernel
 
     public function getCacheDir()
     {
-        return dirname(__DIR__).'/var/cache/'.$this->getEnvironment();
+        return sys_get_temp_dir().'/var/cache/'.$this->getEnvironment();
     }
 
     public function getLogDir()
     {
-        return dirname(__DIR__).'/var/logs';
+        return sys_get_temp_dir().'/var/logs';
     }
 
     public function registerContainerConfiguration(LoaderInterface $loader)
